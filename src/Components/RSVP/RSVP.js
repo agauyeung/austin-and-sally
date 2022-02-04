d3dc0fe0dcfe15387ae1006222f44364d1b299be
agauyeung <agauyeung@dons.usfca.edu>
2022-02-03 23:51:23 -0800

    add rsvp notice

--- src/Components/RSVP/RSVP.js
+++ src/Components/RSVP/RSVP.js
@@ -7,7 +7,7 @@ export default function RSVP() {
       <div className={`panel ${styles["panel"]}`}>
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSevE_Xuc37nDuOZvZv4QpqxrPa1Vax6QlUPZi1vOHyVaBVqcg/viewform?embedded=true"
-          height="2500"
+          height="2995"
           frameBorder="0"
           marginHeight="0"
           marginWidth="0"
@@ -15,6 +15,13 @@ export default function RSVP() {
         >
           Loading…
         </iframe>
+        <div className={styles.help}>
+          If you aren't redirected to a confirmation page after submitting, it
+          unfortunately hasn't gone through. You may have to scroll up to the
+          top of the form to answer the Captcha question. If that doesn't work
+          either, please contact either of us and we will try to help you
+          through it!
+        </div>
       </div>
     </div>
   );
